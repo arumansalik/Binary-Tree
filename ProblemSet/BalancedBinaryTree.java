a038613945c7cb8fcc13915ae30c5472fb2c31fa
unknown <arumansalik45@gmail.com>
2025-10-08 16:01:38 +0530

    Balanced Binary Tree

---
+++ ProblemSet/BalancedBinaryTree.java
@@ -0,0 +1,29 @@
+package ProblemSet;
+
+public class BalancedBinaryTree {
+    public static boolean isBalanced(Node root) {
+        if(root == null) return true;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if(Math.abs(leftHeight - rightHeight) > 1) return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private static int height(Node node) {
+        if(node == null) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println(isBalanced(root));
+    }
+}
--- ProblemSet/maxDepthUsingBFS.java
+++ ProblemSet/maxDepthUsingBFS.java
@@ -1,4 +1,36 @@
 package ProblemSet;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class maxDepthUsingBFS {
+    public static int maxDepth1(Node root) {
+        if(root == null) return 0;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int level = 0;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                Node node = q.poll();
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            level++;
+        }
+        return level;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(4);
+        root.right.left.left = new Node(5);
+        root.right.right = new Node(6);
+
+        int ans = maxDepth1(root);
+        System.out.println(ans);
+    }
 }
