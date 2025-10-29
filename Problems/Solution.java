21a8663c4a85f1e52f2557209b89ea7b091df426
unknown <arumansalik45@gmail.com>
2025-10-29 11:27:35 +0530

    All Nodes Distances at K

--- Problems/Solution.java
+++ Problems/Solution.java
@@ -2,24 +2,24 @@ package Problems;
 import java.util.*;
 
 class Solution {
-    private void markParents(Node root, Map<Node, Node> parent_track) {
+    private static void markParents(Node root, Map<Node, Node> parent_track) {
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
 
         while (!queue.isEmpty()) {
             Node current = queue.poll();
-            if (current.left != null) {
+            if(current.left != null) {
                 parent_track.put(current.left, current);
                 queue.offer(current.left);
             }
-            if (current.right != null) {
+            if(current.right != null) {
                 parent_track.put(current.right, current);
                 queue.offer(current.right);
             }
         }
     }
 
-    public List<Integer> distanceK(Node root, Node target, int k) {
+    public static List<Integer> distanceK(Node root, Node target, int k) {
         Map<Node, Node> parent_track = new HashMap<>();
         markParents(root, parent_track);
 
@@ -28,22 +28,22 @@ class Solution {
         queue.offer(target);
         visited.put(target, true);
 
-        int curr_level = 0;
+        int current_level = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            if (curr_level == k) break;
-            curr_level++;
+            if(current_level == k) break;
+            current_level++;
 
             for (int i = 0; i < size; i++) {
                 Node current = queue.poll();
 
-                if (current.left != null && visited.get(current.left) == null) {
+                if(current.left != null && visited.get(current.left) == null) {
                     queue.offer(current.left);
                     visited.put(current.left, true);
                 }
-                if (current.right != null && visited.get(current.right) == null) {
-                    queue.offer(current.right);
-                    visited.put(current.right, true);
+                if(current.right != null && visited.get(current.right) == null) {
+                      queue.offer(current.right);
+                      visited.put(current.right, true);
                 }
                 if (parent_track.get(current) != null && visited.get(parent_track.get(current)) == null) {
                     queue.offer(parent_track.get(current));
@@ -51,11 +51,27 @@ class Solution {
                 }
             }
         }
-
         List<Integer> result = new ArrayList<>();
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             result.add(queue.poll().data);
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int k = 2;
+        Node root = new Node(3);
+        root.left = new Node(5);
+        root.right = new Node(1);
+        root.left.left = new Node(6);
+        root.left.right = new Node(2);
+        root.right.left = new Node(0);
+        root.right.right = new Node(8);
+        root.left.right.left = new Node(7);
+        root.left.right.right = new Node(4);
+        Node target = root.left;
+
+        List<Integer> result =  distanceK(root, target, k);
+        System.out.println("Node ay distances "  + k + " from target" + target.data + " : " + result);
     }
 }
