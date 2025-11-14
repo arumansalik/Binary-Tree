f654f94e57f91f540fcb5c98dfbc5cb725f71dbb
unknown <arumansalik45@gmail.com>
2025-11-14 22:41:23 +0530

    Recover from Binary Search Tree

--- BinarySearchTree/Problems/RecoverBST.java
+++ BinarySearchTree/Problems/RecoverBST.java
@@ -30,5 +30,29 @@ public class RecoverBST {
                 last = root;
             }
         }
+        prev = root;
+        inOrder(root.right);
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+        printInorder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(3);
+        root.left = new Node(1);
+        root.right = new Node(4);
+        root.right.left = new Node(2);
+
+        System.out.print("Before Fix: ");
+        printInorder(root);
+        System.out.println();
+
+        recoverTree(root);
+
+        System.out.print("After Fix: ");
+        printInorder(root);
     }
 }
